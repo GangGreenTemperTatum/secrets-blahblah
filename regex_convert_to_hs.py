1812af3fa46a910d7a37b1bbfa8b7b25be258b33
GangGreenTemperTatum <104169244+GangGreenTemperTatum@users.noreply.github.com>
2023-05-18 10:46:31 -0700

    fix: regex_str indentation

--- regex_convert_to_hs.py
+++ regex_convert_to_hs.py
@@ -1,16 +1,14 @@
 import re
 
 # Prompt the user to enter a regular expression
-regex_str = input('Enter a regular expression in single quotes ('): ')
-# regex_str = r'(?:\s|=|:|"|^)AKC[a-zA-Z0-9]{10,}'
+regex_str = input('Enter a regular expression: ')
 # Remove enclosing quotes
-regex_str=regex_str.strip('"\'')
+regex_str = regex_str.strip('"\'')
 # Remove unnecessary backslashes
-regex_str=re.sub(r'\\([^\s])', r'\1', regex_str)
+regex_str = re.sub(r'\\([^\s])', r'\1', regex_str)
 # Add any Hyperscan-specific flags, if needed
-flags='i'
+flags = 'i'
 if re.search(r'\.\*', regex_str):
     flags += 's'
 # Print the modified regular expression
-print("Here's your converted Regular Expression, to Hyperscan Regex compatible flag:\n" +
-      regex_str + ' /' + flags)
+print(regex_str + '/' + flags)
